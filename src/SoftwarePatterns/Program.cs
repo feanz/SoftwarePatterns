083b77f6b3f9e21b66b7faf35d8b7b8e500aad3e
Richard Forrest <richard.a.forrest@gmail.com>
2013-08-05 16:27:22 +0100

    Added more examples to console app

--- src/SoftwarePatterns/Program.cs
+++ src/SoftwarePatterns/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using SoftwarePatterns.Core.Adapter;
+using SoftwarePatterns.Core.Bridge;
 using SoftwarePatterns.Core.Builder;
 using SoftwarePatterns.Core.ChainOfResponsability;
 using SoftwarePatterns.Core.EnumerationClass;
@@ -9,8 +11,53 @@ namespace SoftwarePatterns
 {
 	class Program
 	{
-		#region Builder Examples
-		//*****Builder Examples
+		#region Adapter
+
+		//public static void Main()
+		//{
+		//	var sut = new PatternRenderer();
+
+		//	var patterns = new List<Pattern>
+		//		{
+		//			new Pattern { ID = 1, Name = "Builder", Description = "Build stuff"},
+		//			new Pattern { ID = 2, Name = "Adapter", Description = "Makes things fit"}
+		//		};
+
+		//	var result = sut.ListPatterns(patterns);
+
+		//	Console.Write(result);
+
+		//	Console.ReadLine();
+		//}
+
+		#endregion
+
+		#region Bridge
+
+		//public static void Main()
+		//{
+		//	var formatter = new StandardFormatter();
+
+		//	var documents = new List<Manuscript>();
+
+		//	var book = new Book(formatter) {Title = "Title", Author = "Author", Text = "This is some text for the book that is stored in this field it can store a big old load of text in it"};
+		//	var paper = new Paper(formatter) {Class = "A class", Student = "Phill the student", Text = "This is the papers text", References = "ref"};
+		//	var faq = new FAQ(formatter) {Title = "FAQ"};
+		//	faq.Questions.Add("Question One", "Answer one");
+		//	faq.Questions.Add("Question Tow", "Answer Two");
+
+		//	documents.Add(book);
+		//	documents.Add(paper);
+		//	documents.Add(faq);
+
+		//	documents.ForEach(manuscript => manuscript.Print());
+
+		//	Console.ReadLine();
+		//}
+
+		#endregion
+
+		#region Builder
 		//static void Main(string[] args)
 		//{
 		//	Employee employee;
@@ -46,8 +93,37 @@ namespace SoftwarePatterns
 
 		#endregion 
 
+		#region Chain of responsability
+
+		//public static void Main()
+		//{
+		//	var edd = new ExpenseHandler(new ExpenseApprover("Edd", 0));
+		//	var jill = new ExpenseHandler(new ExpenseApprover("Jill", 100));
+		//	var barry = new ExpenseHandler(new ExpenseApprover("Barry", 1000));
+		//	var donald = new ExpenseHandler(new ExpenseApprover("Donald", 10000));
+
+		//	edd.RegisterNext(jill);
+		//	jill.RegisterNext(barry);
+		//	barry.RegisterNext(donald);
+
+		//	var input = Console.ReadLine();
+
+		//	Decimal expenseReportAmount;
+		//	if (decimal.TryParse(input, out expenseReportAmount))
+		//	{
+		//		var expenseReport = new ExpenseReport(expenseReportAmount);
+
+		//		var response = edd.Approve(expenseReport);
+
+		//		Console.WriteLine("This request was {0}.", response);
+		//	}
+
+		//	Console.ReadLine();
+		//}
+
+		#endregion
+
 		#region Enumeration Class
-		//*********EnumerationClass examples
 		//static void Main()
 		//{
 		//	//to stop this sort of code
@@ -81,36 +157,6 @@ namespace SoftwarePatterns
 		//{
 		//	employee.Bonus = employee.Type.BonusSize;
 		//}
-		#endregion
-
-		#region Chain of responsability
-
-		//public static void Main()
-		//{
-		//	var edd = new ExpenseHandler(new ExpenseApprover("Edd", 0));
-		//	var jill = new ExpenseHandler(new ExpenseApprover("Jill", 100));
-		//	var barry = new ExpenseHandler(new ExpenseApprover("Barry", 1000));
-		//	var donald = new ExpenseHandler(new ExpenseApprover("Donald", 10000));
-
-		//	edd.RegisterNext(jill);
-		//	jill.RegisterNext(barry);
-		//	barry.RegisterNext(donald);
-
-		//	var input = Console.ReadLine();
-
-		//	Decimal expenseReportAmount;
-		//	if (decimal.TryParse(input, out expenseReportAmount))
-		//	{
-		//		var expenseReport = new ExpenseReport(expenseReportAmount);
-
-		//		var response = edd.Approve(expenseReport);
-
-		//		Console.WriteLine("This request was {0}.", response);
-		//	}
-
-		//	Console.ReadLine();
-		//}
-
 		#endregion
 	}
 }
